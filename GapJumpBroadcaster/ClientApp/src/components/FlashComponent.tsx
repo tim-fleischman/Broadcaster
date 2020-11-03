dbae209141bf6cb3c9e81f289e4fbf5a02c6b9db
tim-fleischman <58310181+tim-fleischman@users.noreply.github.com>
2020-11-03 08:42:04 +0000

    Adding Rob's component

---
+++ GapJumpBroadcaster/ClientApp/src/components/FlashComponent.tsx
@@ -0,0 +1,40 @@
+﻿import * as React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { ApplicationState } from '../store';
+import * as FlashStore from '../store/FlashComponent';
+
+type FlashProps =
+    FlashStore.BoxState &
+    typeof FlashStore.actionCreators &
+    RouteComponentProps<{}>;
+
+class Flasher extends React.PureComponent<FlashProps> {
+    public render() {
+        return (
+            <React.Fragment>
+                <h1>Box state</h1>
+
+                <p>Rob's React component.</p>
+
+                <p aria-live="polite">Current state: <strong>{this.props.active}</strong></p>
+
+                <button type="button"
+                    className="btn btn-primary btn-lg"
+                    onClick={() => { this.props.boxoff(); }}>
+                    Box Off
+                </button>
+                <button type="button"
+                    className="btn btn-primary btn-lg"
+                    onClick={() => { this.props.boxon(); }}>
+                    Box On
+                </button>
+            </React.Fragment>
+        );
+    }
+};
+
+export default connect(
+    (state: ApplicationState) => state.active,
+    FlashStore.actionCreators
+)(Flasher);
---
+++ GapJumpBroadcaster/ClientApp/src/store/FlashComponent.ts
@@ -0,0 +1,38 @@
+﻿import { Action, Reducer } from 'redux';
+import { AppThunkAction } from './';
+
+export interface BoxState {
+    active: boolean;
+}
+
+export interface BoxAttributes {
+    height: number;
+    width: number;
+    colour: string;
+}
+
+export interface BoxOnAction { type: 'BOX_ON' }
+export interface BoxOffAction { type: 'BOX_OFF' }
+
+export type KnownAction = BoxOnAction | BoxOffAction;
+
+export const actionCreators = {
+    boxon: () => ({ type: 'BOX_ON' } as BoxOnAction),
+    boxoff: () => ({ type: 'BOX_OFF' } as BoxOffAction)
+};
+
+export const reducer: Reducer<BoxState> = (state: BoxState | undefined, incomingAction: Action): BoxState => {
+    if (state === undefined) {
+        return { active: false };
+    }
+
+    const action = incomingAction as KnownAction;
+    switch (action.type) {
+        case 'BOX_ON':
+            return { active: true };
+        case 'BOX_OFF':
+            return { active: false };
+        default:
+            return state;
+    }
+};
